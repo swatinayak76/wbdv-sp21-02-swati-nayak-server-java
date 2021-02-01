f8dfa3b219e41ca0ed290a832c6278aa4d4c1d56
swatinayak76 <swati.nayak76@gmail.com>
2021-02-01 14:21:00 -0800

    First Commit

---
+++ src/main/java/com/example/wbdvsp2102swatinayakserverjava/WbdvSp2102SwatiNayakServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2102swatinayakserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbdvSp2102SwatiNayakServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbdvSp2102SwatiNayakServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wbdvsp2102swatinayakserverjava/WbdvSp2102SwatiNayakServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2102swatinayakserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbdvSp2102SwatiNayakServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
